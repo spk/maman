f95859a1859da1603447fcb19d6976e160c85c87
Laurent Arnoud <laurent@spkdev.net>
2017-05-09 19:44:59 +0200

    Update clippy and add html_root_url

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@
 //!
 //! * `MAMAN_ENV`=development
 //! * `REDIS_URL`="redis://127.0.0.1/"
+#![doc(html_root_url = "https://docs.rs/maman/0.11.0")]
 #![deny(warnings)]
 #![cfg_attr(feature = "clippy", feature(plugin))]
 #![cfg_attr(feature = "clippy", plugin(clippy))]
