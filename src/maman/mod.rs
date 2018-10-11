1fe2331ec7524a137eda3a085b501017cf8229e0
Laurent Arnoud <laurent@spkdev.net>
2018-10-11 22:06:21 +0200

    Rustfmt

--- src/maman/mod.rs
+++ src/maman/mod.rs
@@ -61,7 +61,10 @@ impl<'a> Spider<'a> {
         }
     }
 
-    #[cfg_attr(feature = "cargo-clippy", allow(unknown_lints, clippy::needless_pass_by_value))]
+    #[cfg_attr(
+        feature = "cargo-clippy",
+        allow(unknown_lints, clippy::needless_pass_by_value)
+    )]
     pub fn visit_page(&mut self, page: Page) {
         self.visited_urls.push(page.url.clone());
         for u in &page.urls {
