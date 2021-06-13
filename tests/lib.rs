ba0d9e3e9a37b9ffb089edd4596e6d3a00230ff9
Laurent Arnoud <laurent@spkdev.net>
2021-06-13 14:05:37 +0200

    clippy: redundant clone

--- tests/lib.rs
+++ tests/lib.rs
@@ -76,12 +76,7 @@ fn test_json_job_format() {
     let url = Url::parse("http://example.net/").unwrap();
     let mut headers = BTreeMap::new();
     headers.insert("content-type".to_string(), "text/html".to_string());
-    let page = Page::new(
-        url,
-        input.to_string(),
-        headers.clone(),
-        "200 OK".to_string(),
-    );
+    let page = Page::new(url, input.to_string(), headers, "200 OK".to_string());
     let page_object = page.as_object();
     let job = page.to_job();
     assert_eq!(job.class, maman_name!());
